47ab6b94e88c61d21190bf42deb37f8f14b7f7a7
Cory Virok <coryvirok@gmail.com>
2012-10-29 18:38:10 -0700

    settings changes

--- stashboard/settings.py
+++ stashboard/settings.py
@@ -2,17 +2,17 @@ import os
 
 DEBUG = False
 
-SITE_NAME = "Stashboard"
-SITE_AUTHOR = "Colonel Mustache"
-SITE_URL = "http://stashbooard.appspot.com"
-REPORT_URL = "mailto:help@stashboard.org"
+SITE_NAME = "Ratchet.io Status"
+SITE_AUTHOR = "Cory Virok"
+SITE_URL = "http://ratchetstatus.appspot.com"
+REPORT_URL = "mailto:help@ratchet.io"
 
 # Twitter update settings
 TWITTER_CONSUMER_KEY = ''
 TWITTER_CONSUMER_SECRET = ''
 TWITTER_ACCESS_TOKEN = ''
 TWITTER_ACCESS_TOKEN_SECRET = ''
-TWITTER_HANDLE = 'stashboard_status'
+TWITTER_HANDLE = 'ratchetio'
 
 # RSS Feed settings
 RSS_NUM_EVENTS_TO_FETCH = 50
@@ -23,4 +23,4 @@ CONSUMER_SECRET = 'anonymous'
 
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), "templates"),
-    )
+)
